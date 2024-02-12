1b56191b2eefef7543a1f857288846478ab7ba23
Roman <46721537+XRolandX@users.noreply.github.com>
2024-02-12 17:16:52 +0200

    BufferSystem implemented.
    Mathematics.Random added to AddComponentSystem.
    Next i want to remake SpawnEntities as :SystemBase

--- Assets/GAME SCENES/Scenes/Scene 3/Scripts/CubeMovementSystem.cs
+++ Assets/GAME SCENES/Scenes/Scene 3/Scripts/CubeMovementSystem.cs
@@ -2,13 +2,16 @@ using Unity.Entities;
 using Unity.Transforms;
 using Unity.Mathematics;
 
+
 public partial class CubeMovementSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-        //float deltaTime = Time.DeltaTime;
+        float deltaTime = Time.DeltaTime;
 
-       // Entities.ForEach((ref Translation translation) => {})
-            //.ScheduleParallel();
+        Entities.ForEach((ref Translation translation, in MoveSpeed moveSpeed) =>
+        {
+            translation.Value += new float3(0, moveSpeed.Value * deltaTime, 0);
+        }).ScheduleParallel();
     }
 }
