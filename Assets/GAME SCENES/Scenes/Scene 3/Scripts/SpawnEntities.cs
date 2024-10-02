89ba49ecc9f6fcff300eed397ae33a112ec3ba02
Roman <46721537+XRolandX@users.noreply.github.com>
2024-10-02 14:23:57 +0300

    ss

---
+++ Assets/GAME SCENES/Scenes/Scene 3/Scripts/SpawnEntities.cs
@@ -0,0 +1,74 @@
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Physics;
+using UnityEngine;
+using Unity.Mathematics;
+
+public partial class SpawnEntities : SystemBase
+{
+    private EndSimulationEntityCommandBufferSystem _ecbSystem;
+    private BlobAssetStore _blobAssetStore;
+    private Entity _prefabEntity;
+    private GameObject cubePrefab;
+    public PlayerControls controls;
+    private float spawnTimer = 0f;
+    private readonly float entityForce = 100f;
+
+    protected override void OnCreate()
+    {
+        controls = new PlayerControls();
+        controls.Enable();
+
+        cubePrefab = Resources.Load<GameObject>("New Prefab");
+
+        _ecbSystem = World
+            .GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+
+        _blobAssetStore = new BlobAssetStore();
+    }
+    protected override void OnUpdate()
+    {
+        var deltaTime = Time.DeltaTime;
+        spawnTimer += deltaTime;
+
+        var mouseInput = GetSingleton<MouseInput>();
+        var spawnPosition = GetSingleton<SpawnPosition>().Position;
+        var spawnRotation = GetSingleton<SpawnRotation>().Rotation;
+
+        if (mouseInput.LeftClickPerformed && spawnTimer >= 0.1f)
+        {
+            spawnTimer = 0f;
+            var ecb = _ecbSystem.CreateCommandBuffer();
+
+            if(cubePrefab != null)
+            {
+                var settings = GameObjectConversionSettings
+            .FromWorld(World.DefaultGameObjectInjectionWorld, _blobAssetStore);
+                _prefabEntity = GameObjectConversionUtility
+                    .ConvertGameObjectHierarchy(cubePrefab, settings);
+
+                float3 forwardDirection = math.mul(spawnRotation, new float3(0, 0, 1));
+
+                Entity instance = ecb.Instantiate(_prefabEntity);
+                ecb.SetComponent(instance, new Translation { Value = spawnPosition });
+                ecb.SetComponent(instance, new Rotation { Value = spawnRotation });
+                ecb.AddComponent(instance, new PhysicsVelocity
+                {
+                    Linear = forwardDirection * entityForce,
+                    Angular = float3.zero
+                });
+
+                _ecbSystem.AddJobHandleForProducer(Dependency);
+            }
+            else
+            {
+                Debug.LogError("Cube prefab is null");
+            }
+        }
+    }
+    protected override void OnDestroy()
+    {
+        _blobAssetStore.Dispose();
+        cubePrefab = null;
+    }
+}
