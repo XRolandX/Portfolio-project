791b70e62ed6017de9abc1479aea4319e8a93698
Roman <46721537+XRolandX@users.noreply.github.com>
2024-10-02 12:54:42 +0300

    upd scene 1

--- Assets/GAME SCENES/Scenes/Scene 1/Scripts/CharacterMovement.cs
+++ Assets/GAME SCENES/Scenes/Scene 1/Scripts/CharacterMovement.cs
@@ -1,23 +1,22 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CharacterMovement : MonoBehaviour
 {
+    public CharacterController characterController;
     private PlayerControls playerControls;
     public Transform groundChecker;
     public Joystick moveJoystick;
-    public Button jumpButton;
 
     private Vector3 gravityVelocity;
     public LayerMask groundMask;
     private Vector2 moveInput;
 
-    readonly float groundCheckerDistance = 0.4f;
-    public readonly float gravity = -9.81f;
-    readonly float movementSpeed = 12f;
-    readonly float jumpPower = 1f;
+    [SerializeField] private float movementSpeed = 12f;
+    [SerializeField] private float jumpPower = 1f;
+    [SerializeField] private bool isGrounded;
 
-    [SerializeField] bool isGrounded;
+    private readonly float groundCheckerDistance = 0.1f;
+    public readonly float gravity = -9.81f * 4.0f;
 
     private void Awake()
     {
@@ -25,49 +24,50 @@ public class CharacterMovement : MonoBehaviour
         playerControls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
         playerControls.Player.Move.canceled += ctx => moveInput = Vector2.zero;
     }
-<<<<<<< Updated upstream
-    private void Awake()
-    {
 
-        PlayerControlsInitialisation();
-    }
-=======
->>>>>>> Stashed changes
     void Update()
     {
         Gravity();
 
-    #if UNITY_ANDROID
-        MoveWithJoystick();
-    #endif
+#if UNITY_ANDROID
+            MoveWithJoystick();
+#endif
 
-    #if UNITY_STANDALONE_WIN
+#if UNITY_STANDALONE_WIN
         MoveWithKeyboard();
         JumpKeyboardButton();
-    #endif
+#endif
     }
+
     public void Gravity()
     {
-        isGrounded = Physics.Raycast(groundChecker.position, Vector3.down, groundCheckerDistance, groundMask);
+        isGrounded = Physics.CheckSphere(groundChecker.position, groundCheckerDistance, groundMask);
 
-        if (!isGrounded)
+        if (isGrounded && gravityVelocity.y < 0)
+        {
+            gravityVelocity.y = -2f;
+        }
+        else
         {
             gravityVelocity.y += gravity * Time.deltaTime;
-            transform.Translate(gravityVelocity * Time.deltaTime);
         }
+
+        characterController.Move(gravityVelocity * Time.deltaTime);
     }
 
     private void MoveWithKeyboard()
     {
-        Vector3 move = movementSpeed * Time.deltaTime * new Vector3(moveInput.x, 0f, moveInput.y);
-        transform.Translate(move, Space.Self);
+        _ = new Vector3(moveInput.x, 0f, moveInput.y);
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+
+        characterController.Move(movementSpeed * Time.deltaTime * move);
     }
 
     private void JumpKeyboardButton()
     {
         if (playerControls.Player.Jump.triggered && isGrounded)
         {
-            gravityVelocity.y = Mathf.Sqrt(jumpPower * -2 * gravity); // formula of jump square is root of height * -2 * gravity
+            gravityVelocity.y = Mathf.Sqrt(jumpPower * -2 * gravity);
         }
     }
 
@@ -79,14 +79,17 @@ public class CharacterMovement : MonoBehaviour
             float x = moveJoystick.Horizontal;
             float z = moveJoystick.Vertical;
             Vector3 move = transform.right * x + transform.forward * z;
-            transform.Translate(move, Space.Self);
+
+            // Переміщення за допомогою CharacterController
+            characterController.Move(movementSpeed * Time.deltaTime * move);
         }
     }
+
     public void JumpUIButton()
     {
         if (isGrounded)
         {
-            gravityVelocity.y = Mathf.Sqrt(jumpPower * -2 * gravity);
+            gravityVelocity.y = Mathf.Sqrt(jumpPower * -2 * gravity);  // Стрибок через UI кнопку
         }
     }
     #endregion
